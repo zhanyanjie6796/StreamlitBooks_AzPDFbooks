8e89d6ec9cdeb1e0bc5e71dbbb21f6db41040611
zhanyanjie6796 <jack6796@gmail.com>
2023-05-18 14:33:24 +0800

    update

--- home.py
+++ home.py
@@ -1,13 +1,12 @@
 import streamlit as st
-st.markdown('# DEMO')
-st.markdown('# Azure openAI langchain PDF')
+st.markdown('# Azure PDF Books')
+st.markdown('### 演示 Azure Open AI LangChain 多 PDF 的嵌入和查詢')
 st.markdown('----')
-st.markdown('## 這裏可以演示 langchain PDF 的編碼和查詢')
 
 # 測試環境使用 AZURE_OPENAI_API_KEY 設定在 Streamlit 後臺。
-# import os
-# if os.getenv("AZURE_OPENAI_API_KEY") is not None:
-#     st.session_state['AZURE_OPENAI_API_KEY'] = os.getenv("AZURE_OPENAI_API_KEY")
+import os
+if os.getenv("AZURE_OPENAI_API_KEY") is not None:
+    st.session_state['AZURE_OPENAI_API_KEY'] = os.getenv("AZURE_OPENAI_API_KEY")
 
 # 輸入 AZURE_OPENAI_API_KEY  
 if 'AZURE_OPENAI_API_KEY' not in st.session_state or st.session_state['AZURE_OPENAI_API_KEY'] == "": 
@@ -16,4 +15,5 @@ if 'AZURE_OPENAI_API_KEY' not in st.session_state or st.session_state['AZURE_OPE
 
 if st.session_state['AZURE_OPENAI_API_KEY'] != "":
     st.write('您的輸入的 AZURE OPENAI_API_KEY：', st.session_state['AZURE_OPENAI_API_KEY']) 
+    st.write('您可以開始使用本網頁的功能了。')
     st.write('如果要重新輸入，請按 F5 重新整理網頁。')
